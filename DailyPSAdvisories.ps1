d6b1c7177b9f61edb46a64f4941fa9d6a2796de1
keyboardcrunch <40863898+keyboardcrunch@users.noreply.github.com>
2018-12-12 08:57:18 -0600

    html formatting fixes

--- DailyPSAdvisories.ps1
+++ DailyPSAdvisories.ps1
@@ -24,16 +24,50 @@ ForEach ($Item in $FeedXml.rss.channel.item) {
     }
 }
 
-$FeedData = $Entries | Format-List
+$style = @"
+    <style>
+    @charset "UTF-8";
+
+    table
+    {
+    font-family:"Trebuchet MS", Arial, Helvetica, sans-serif;
+    border-collapse:collapse;
+    }
+    td
+    {
+    font-size:1em;
+    border:1px solid #98bf21;
+    padding:5px 5px 5px 5px;
+    }
+    th
+    {
+    font-size:1.1em;
+    text-align:center;
+    padding-top:5px;
+    padding-bottom:5px;
+    padding-right:7px;
+    padding-left:7px;
+    background-color:#A7C942;
+    color:#ffffff;
+    }
+    name tr
+    {
+    color:#F00000;
+    background-color:#EAF2D3;
+    }
+    </style>
+"@
+
+[string]$FeedData = $Entries | Select-Object Title, Description, Comments | ConvertTo-HTML -Head $style
 $FeedTitle = $($FeedXml.rss.channel.title -replace "Files Date: ") + " Advisories"
 
 $Params = @{
     'Body' = $FeedData
     'BodyAsHtml' = $true
-    'From' = "advisories@example.com"
+    'From' = "test@example.com"
     'SmtpServer' = "smtp.example.com"
     'Subject' = $Feedtitle
-    'To' = "alerts@example.com"
+    'To' = "test@example.com"
 }
 
 Send-MailMessage @Params
